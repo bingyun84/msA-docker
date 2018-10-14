0aab0363921cfea680369de0d0874feadb6ec643
liming6 <liming6@asiainfo.com>
2018-10-14 19:32:50 +0800

    用户信息功能开发

---
+++ user-edge-service/src/main/java/com/idig8/user/thrift/ServiceProvider.java
@@ -0,0 +1,65 @@
+package com.idig8.user.thrift;
+
+import com.idig8.thrift.message.MessageService;
+import com.idig8.thrift.user.UserService;
+import org.apache.thrift.TServiceClient;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServiceProvider {
+
+    @Value("${thrift.user.ip}")
+    private String serverIp;
+
+    @Value("${thrift.user.port}")
+    private  int serverPort;
+
+    @Value("${thrift.message.ip}")
+    private String messageServerIp;
+
+    @Value("${thrift.message.port}")
+    private int messageServerPort;
+
+    private enum ServiceType {
+        USER,
+        MESSAGE
+    }
+
+    public UserService.Client getUserService() {
+
+        return getService(serverIp, serverPort, ServiceType.USER);
+    }
+
+    public MessageService.Client getMessasgeService() {
+
+        return getService(messageServerIp, messageServerPort, ServiceType.MESSAGE);
+    }
+
+    public <T> T getService(String ip, int port, ServiceType serviceType) {
+        TSocket socket = new TSocket(ip, port, 3000);
+        TTransport transport = new TFramedTransport(socket);
+        try {
+            transport.open();
+        } catch (TTransportException e) {
+            e.printStackTrace();
+            return null;
+        }
+        TProtocol protocol = new TBinaryProtocol(transport);
+
+        TServiceClient result = null;
+        switch (serviceType) {
+            case USER:
+                result = new UserService.Client(protocol);
+                break;
+            case MESSAGE:
+                result = new MessageService.Client(protocol);
+                break;
+        }
+        return (T)result;
+    }
+
+}
