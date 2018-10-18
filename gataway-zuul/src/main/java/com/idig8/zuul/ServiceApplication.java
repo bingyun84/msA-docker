276a02a74501f445f7349fb51e7cdc3eaa9b8704
liming6 <liming6@asiainfo.com>
2018-10-18 13:04:09 +0800

    springboot1.3 引入springcloud pom修复

--- gataway-zuul/src/main/java/com/idig8/zuul/ServiceApplication.java
+++ gataway-zuul/src/main/java/com/idig8/zuul/ServiceApplication.java
@@ -1,11 +1,11 @@
-package com.idig8.zuul;
+package com.imooc.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
- * Created by liming
+ * Created by Michael on 2017/10/28.
  */
 @SpringBootApplication
 @EnableZuulProxy
